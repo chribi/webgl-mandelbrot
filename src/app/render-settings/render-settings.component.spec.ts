e9c15d4d985e2350003169f1a4f8ea91bad56057
Christoph Bier <christoph.bier@gmx.de>
2018-04-02 11:55:15 +0200

    Fix tests

--- src/app/render-settings/render-settings.component.spec.ts
+++ src/app/render-settings/render-settings.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RenderSettingsComponent } from './render-settings.component';
+import { FormsModule } from '@angular/forms';
 
 describe('RenderSettingsComponent', () => {
   let component: RenderSettingsComponent;
@@ -8,7 +9,8 @@ describe('RenderSettingsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RenderSettingsComponent ]
+      declarations: [ RenderSettingsComponent ],
+      imports: [ FormsModule ]
     })
     .compileComponents();
   }));
